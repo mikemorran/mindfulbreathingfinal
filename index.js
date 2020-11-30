d67e8e312cea5634526b0906e11b1328446f8515
Michael Morran <michaelmorran@Michaels-MacBook-Pro-6.local>
2020-11-30 13:28:05 -0600

    Big Update

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ let Datastore = require('nedb');
 let db = new Datastore('breathData.db');
 db.loadDatabase();
 
+
 // Initialize http
 let http = require('http');
 let server = http.createServer(app);
@@ -47,9 +48,9 @@ app.get('/getBreaths', (req, res) => {
 
 // Process POST requests and insert data whenever a user completes a breath
 app.post('/sendBreaths', (req, res) => {
-    console.log(req.body.name);
+    console.log(req.body);
     db.find({"name" : req.body.name}, (err, docs) => {
-        console.log(docs);
+        // console.log(docs);
         if (!docs[0]) {
             db.insert(req.body, (err, newDocs) => {
                 if (err) {
@@ -58,10 +59,21 @@ app.post('/sendBreaths', (req, res) => {
                     console.log("new user data added");
                     res.json({"task" : "success"});
                 }
+                db.persistence.compactDatafile();
             });
         } else {
-            db.update({"name" : req.body.name}, req.body, {multi: true}, (err, numReplaced) => {
-                console.log("userData updated");
+            console.log({"name" : req.body.name})
+            let jsonQuery = JSON.stringify({"name" : req.body.name});
+            console.log(jsonQuery);
+            db.update({"name" : req.body.name}, req.body, {}, (err, numReplaced, upsert) => {
+                if (err) {
+                    console.log("error");
+                    res.json('failed');
+                } else {
+                    res.json({"task" : "success"});
+                }
+                console.log(numReplaced, upsert);
+                db.persistence.compactDatafile();
             });
         }
     })
@@ -79,5 +91,23 @@ io.sockets.on('connection', function(socket) {
         // console.log(userObject);
         socket.broadcast.emit('msg', newBreaths);
     });
+
+    socket.on('bet', function(bet) {
+        // console.log(bet);
+        socket.broadcast.emit('bet', bet);
+    });
+
+    socket.on('challengeAccepted', function(challengeObject) {
+        console.log(challengeObject);
+        io.sockets.emit('challengeAccepted', challengeObject);
+    });
+
+    socket.on('bettingOver', function(scoreObject) {
+        console.log(scoreObject);
+        socket.broadcast.emit('bettingOver', scoreObject);
+    });
 });
 
+// db.remove({"_id":"Atuf9xwlq4dTrVWk"}, {}, function (err, numRemoved) {
+//     // numRemoved = 1
+//   });
